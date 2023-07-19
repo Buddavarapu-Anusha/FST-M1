c1eea1ee8709ce25df2f732b0f68bcca32e11060
Buddavarapu-Anusha <96100464+Buddavarapu-Anusha@users.noreply.github.com>
2023-07-20 01:11:53 +0530

    Add files via upload

--- Java/Activity14.java
+++ Java/Activity14.java
@@ -1,39 +1,30 @@
-package activites;
-
 import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-
-
+ 
 public class Activity14 {
     public static void main(String[] args) throws IOException {
         try {
-            File file = new File("C:\\Users\\KanakavalliRamyasri\\Desktop\\FULL STACK TESTING\\New folder\\hello.txt");
+            File file = new File("src/session4/newfile.txt");
             boolean fStatus = file.createNewFile();
-            if(fStatus) {
+            if(fStatus)
+            {
                 System.out.println("File created successfully!");
-            } else {
+            } else 
+            {
                 System.out.println("File already exists at this path.");
             }
-
-            //get the file Object
-            File fileUtil = FileUtils.getFile("C:\\Users\\BandhanPal\\Desktop\\FULL STACK TESTING\\New folder\\hello.txt");
-            //Read file
+            File fileUtil = FileUtils.getFile("src/session4/newfile.txt");
             System.out.println("Data in file: " + FileUtils.readFileToString(fileUtil, "UTF8"));
-
-            //Create directory
-            File destDir = new File("New Folder");
-            //Copy file to directory
+ 
+            File destDir = new File("resources");
             FileUtils.copyFileToDirectory(file, destDir);
-
-            //Get file from new directory
-            File newFile = FileUtils.getFile(destDir, "hello.txt");
-            //Read data from file
+            File newFile = FileUtils.getFile(destDir, "newfile.txt");
             String newFileData = FileUtils.readFileToString(newFile, "UTF8");
-            //Print it
             System.out.println("Data in new file: " + newFileData);
-        } catch(IOException errMessage) {
+        } catch(IOException errMessage)
+        {
             System.out.println(errMessage);
         }
     }
